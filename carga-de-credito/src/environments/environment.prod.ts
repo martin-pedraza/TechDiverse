b1e33b0debf727eac2fec20ec943a8fd7db2d71a
Martin Pedraza <56758686+martin-pedraza@users.noreply.github.com>
2024-01-18 15:24:39 -0300

    Update environment.prod.ts

--- carga-de-credito/src/environments/environment.prod.ts
+++ carga-de-credito/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'carga-de-credito-7b46a',
-    appId: '1:712736283281:web:45346343a3b6a46e303d30',
-    storageBucket: 'carga-de-credito-7b46a.appspot.com',
-    apiKey: 'AIzaSyBFveumuaaWSPcOjF9aoDffQR4iP3d6JFU',
-    authDomain: 'carga-de-credito-7b46a.firebaseapp.com',
-    messagingSenderId: '712736283281',
+    apiKey: "AIzaSyAWFjDUjvgj4KyPTeU0vuVBycg7QVrDQj4",
+    authDomain: "pps-aplicaciones.firebaseapp.com",
+    projectId: "pps-aplicaciones",
+    storageBucket: "pps-aplicaciones.appspot.com",
+    messagingSenderId: "63364640193",
+    appId: "1:63364640193:web:3c7428d94355c57c2b9340",
   },
   production: true
 };
